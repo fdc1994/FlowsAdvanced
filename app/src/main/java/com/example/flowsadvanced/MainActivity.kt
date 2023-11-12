a9c2d07b48f65f10e6eb80934ead7aee78f5e512
Fábio de Carvalho <fdc1994@hotmail.com>
2023-11-12 18:40:06 +0100

    add flows information and examples

--- app/src/main/java/com/example/flowsadvanced/MainActivity.kt
+++ app/src/main/java/com/example/flowsadvanced/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.flowsadvanced
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.flowsadvanced.ui.theme.FlowsAdvancedTheme
-import kotlinx.coroutines.flow.collect
 
 class MainActivity : ComponentActivity() {
     private val viewModel : MainViewModel by viewModels()
@@ -22,13 +22,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FlowsAdvancedTheme {
-                val time = viewModel.countdownFlows.collectAsState(initial = 10)
+                val time = viewModel.countdownFlow.collectAsState(initial = 10)
                 // A surface container using the 'background' color from the theme
                Box(modifier = Modifier.fillMaxSize()) {
                    Text(
-                       text = time.value.toString()
+                       text = time.value.toString(),
+                       fontSize= 30.sp,
+                       modifier = Modifier.align(Alignment.Center)
                    )
                }
+                if(time.value == 0) {
+                    Toast.makeText(this, "Finished Timer", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
--- app/src/main/java/com/example/flowsadvanced/MainViewModel.kt
+++ app/src/main/java/com/example/flowsadvanced/MainViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.flowsadvanced
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    val countdownFlows = flow<Int> {
+    val countdownFlow = flow<Int> {
         val startingValue = 10
         var currentValue = startingValue
         emit(startingValue)
@@ -15,6 +18,40 @@ class MainViewModel : ViewModel() {
             delay(1000L)
             currentValue--
             emit(currentValue)
+        }
+    }
+    init {
+        collectFlow()
+        collectLatestFlow()
+    }
+    private fun collectFlow() {
+        /**
+         * Collect will always get all the states regardless of whether the flow has already emitted
+         * and started a new emission.
+         */
+        viewModelScope.launch {
+            countdownFlow.collect {time ->
+                delay(1200)
+                println("Collect : The current time is $time")
+
+            }
+        }
+    }
+
+    private fun collectLatestFlow() {
+        /**
+         * Collect Latest will only collect current emissions.
+         *
+         * In this case, since we are emmitting a new value every second, but delaying this collection by 1.5
+         * seconds, only the last value will be printed since no new valus will be emitted after it
+         * and we can collect it after 1.5 seconds
+         */
+        viewModelScope.launch {
+            countdownFlow.collectLatest {time ->
+                delay(1500)
+                println("Collect latest : The current time is $time")
+
+            }
         }
     }
 
